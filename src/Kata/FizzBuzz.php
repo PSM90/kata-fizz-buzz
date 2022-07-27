926f93f0d1b7c6d64cd38ebd3cb484123e87b795
PSM90 <papasalvatoremirko@gmail.com>
2022-07-27 20:35:54 +0200

    FizzBuzz handle logic

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -4,8 +4,20 @@ namespace Kata;
 
 class FizzBuzz
 {
-    public function handle(): bool
+    public function handle($values)
     {
-        return true;
+    	$return_array = [];
+    	foreach($values as $value){
+    		if ($value % 3 == 0 && $value % 5 == 0)
+    			$return_array[] = 'FizzBuzz';
+    		elseif ($value % 3 == 0)
+				$return_array[] = 'Fizz';
+    		elseif ($value % 5 == 0)
+				$return_array[] = 'Buzz';
+    		else
+    			$return_array[] = $value;
+		}
+
+        return $return_array;
     }
 }
