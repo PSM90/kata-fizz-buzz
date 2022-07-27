bc4b9c25c5fc36b5d8a88e7aa3879ffc631093ca
PSM90 <papasalvatoremirko@gmail.com>
2022-07-27 20:36:43 +0200

    Phpunit test - mod for correct result

--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -3,7 +3,6 @@
 namespace Kata;
 
 use PHPUnit\Framework\TestCase;
-use Kata\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
@@ -16,11 +15,9 @@ class FizzBuzzTest extends TestCase
 
     public function testShallPass(): void
     {
-        $this->assertEquals(1, 1);
-    }
-
-    public function testHandleReturnTrue(): void
-    {
-        $this->assertEquals(true, $this->fizzBuzz->handle());
+    	$values = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ];
+    	$expected_vals = [ 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz' ];
+    	$processed_values = $this->fizzBuzz->handle($values);
+        $this->assertEquals($expected_vals, $processed_values);
     }
 }
